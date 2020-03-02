253800336ade4b8c3b70500c8401bd8d8cf0e0e6
akshay <akshayphate@gmail.com>
2020-03-02 10:58:14 +0530

    Corona Virus Tracker

---
+++ src/main/java/io/akshay/coronavirustracker/models/LocationStat.java
@@ -0,0 +1,40 @@
+package io.akshay.coronavirustracker.models;
+
+public class LocationStat {
+    private String state;
+    private String country;
+    private int latestReportedCases;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestReportedCases() {
+        return latestReportedCases;
+    }
+
+    public void setLatestReportedCases(int latestReportedCases) {
+        this.latestReportedCases = latestReportedCases;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStat{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestReportedCases=" + latestReportedCases +
+                '}';
+    }
+}
--- src/main/java/io/akshay/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/io/akshay/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,5 +1,6 @@
 package io.akshay.coronavirustracker.services;
 
+import io.akshay.coronavirustracker.models.LocationStat;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,15 +13,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
 
     public static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
 
+    public List<LocationStat> allStats = new ArrayList<>();
+
     @PostConstruct
     @Scheduled(cron = "5 * * * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStat> newStat = new ArrayList<>();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest
                 .newBuilder(URI.create(VIRUS_DATA_URL))
@@ -29,9 +35,13 @@ public class CoronaVirusDataService {
         StringReader reader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(reader);
         for (CSVRecord record : records) {
-            String state = record.get("Province/State");
-            System.out.println(state);
+            LocationStat stat = new LocationStat();
+            stat.setState(record.get("Province/State"));
+            stat.setCountry(record.get("Country/Region"));
+            stat.setLatestReportedCases(Integer.parseInt(record.get(record.size()-1)));
+            System.out.println(stat);
+            newStat.add(stat);
         }
-
+        allStats = newStat;
     }
 }
